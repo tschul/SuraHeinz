58776ab91c6f0aed146c345d5dadf82e9dae85bc
tschul <tschul@hotmail.com>
2017-05-06 23:11:15 +0200

    list for query

--- src/reddit.py
+++ src/reddit.py
@@ -11,17 +11,12 @@ class RedditPRAW:
         self._reddit = authenticate_reddit(self._cfg)
 
     def search_and_pick(self, subreddit, query):
-<<<<<<< HEAD
         if ',' in query:
             query = random.choice(cfg.reddit_query.split(','))
 
         res_gen = self.reddit.subreddit(subreddit).search(query)
 
-=======
-        res_gen = self._reddit.subreddit(subreddit).search(query)
->>>>>>> 1af9ce7281874b1ecca48153e2eca0946d93d979
         r = dict()
-
         try:
             for idx, res in enumerate(res_gen):
                 r[idx] = res
